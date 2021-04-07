97bfbdb5012bab11433014e58077b1082651f001
nari <oinari.work@gmail.com>
2021-04-07 15:51:17 +0900

    Twitterカード設定変更

--- app/resources/views/index.blade.php
+++ app/resources/views/index.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <meta name="twitter:card" content="summary">
+  <meta name="twitter:card" content="summary_large_image">
   <meta property="og:url" content="https://yourtube.co.jp/">
   <meta property="og:title" content="YourTube">
   <meta property="og:description" content="好みのジャンルを選んで、あなたにオススメ動画を検索する！">
